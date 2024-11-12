eb1ef1b4b4acbbfeea730497f72865fc8d0a4942
n01e0 <reoshiseki@gmail.com>
2024-11-12 10:24:30 +0900

    fix php-access

--- php-access/www/info.php
+++ php-access/www/info.php
@@ -1,9 +1,11 @@
 <?php
-    session_start();
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
     header("X-XSS-Protection: 0;");
     require 'common.php';
 
-    if (isset($_SESSION['id'])) {
+    if (!isset($_SESSION['id'])) {
         header("Location: / ");
         exit();
     }
